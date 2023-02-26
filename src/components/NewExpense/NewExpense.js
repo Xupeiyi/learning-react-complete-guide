358703e1c4277cc16c252ddb0e4f475f87debdb5
Peiyi Xu <xu_py311@outlook.com>
2023-02-26 17:09:22 -0500

    created "Add New Expense" button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,20 @@
 import './NewExpense.css';
 
 import ExpenseForm from './ExpenseForm';
+import {useState} from "react";
 
 const NewExpense = (props) => {
+
+    const [displayForm, setDisplayForm] = useState(false);
+
+    const addNewExpenseHandler = () => {
+        setDisplayForm(true);
+    }
+
+    const cancelHandler = () => {
+        setDisplayForm(false);
+    }
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -10,12 +22,16 @@ const NewExpense = (props) => {
         };
         console.log(expenseData);
         props.onAddExpense(expenseData);
+        setDisplayForm(false);
     };
 
+
     return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-        </div>
+            <div className="new-expense">
+                {displayForm?
+                    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={cancelHandler}/> :
+                    <button type='button' onClick={addNewExpenseHandler}>Add New Expense</button>}
+            </div>
     )
 };
 
